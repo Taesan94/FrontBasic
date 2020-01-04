4f683febed13e20819b659f06174f047d0f90283
NAME <eamil@naver.com>
2020-01-04 21:56:15 +0900

    로컬저장소를 활용하여 사용자 정보 저장 test

--- js/clock.js
+++ js/clock.js
@@ -7,6 +7,7 @@ function getTime() {
 	const hours = date.getHours();
 	const seconds = date.getSeconds();
 	
+	// 아래 백틱문자(`)를 사용한 문법은 문자열 표현형식 중 템플릿 리터럴 개념..!
 	clockTtile.innerText = `${hours < 10 ? `0${hours}` : hours }:${
 	 minutes < 10 ? `0${minutes}` : minutes 
 	 }:${ seconds < 10 ? `0${seconds}` : seconds }`;
